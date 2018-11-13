d996b55c362cc2605f7fbb554faa683a0904bf55
Henri Durliat <henri.durliat@student-cs.fr>
2018-11-13 16:57:30 +0100

    nouvelle fonction rotation image à tester

--- Code/facerecognition/utils-cv.py
+++ Code/facerecognition/utils-cv.py
@@ -7,3 +7,9 @@ def load_and_display_image(filename):
     cv2.waitKey(0) & 0xFF
     cv2.destroyAllWindows()
 
+def process_image(filename):
+    img = cv2.imread(filename,0)
+    rows,cols = img.shape
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+    dst = cv2.warpAffine(img,M,(cols,rows))
+
