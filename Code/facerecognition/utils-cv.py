66dbd6d0f49d0b1d8e9c9640dfc526c3f9adc29d
Lugagne Morgan <morgan.lugagne@student-cs.fr>
2018-11-14 09:41:34 +0100

    Ajout des fonctions enregistrement et transformation NB

--- Code/facerecognition/utils-cv.py
+++ Code/facerecognition/utils-cv.py
@@ -1,5 +1,7 @@
 import cv2
 
+chemin_morgan="C:/Users/proprietaire/Desktop/codingweek/facerecognition/facerecognition/Code/Data"
+
 def load_and_display_image(filename):
     """Fonction permettant à partir du chemin d'acces filename, de charger et d'afficher l'image."""
     img=cv2.imread(filename)
@@ -7,13 +9,20 @@ def load_and_display_image(filename):
     cv2.waitKey(0) & 0xFF
     cv2.destroyAllWindows()
 
+def save_image(img,filename):
+    cv2.imwrite(filename,img)
+
 def process_image_rotation(filename):
     img = cv2.imread(filename,0)
     rows,cols = img.shape
     M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
     dst = cv2.warpAffine(img,M,(cols,rows))
-    cv2.imshow("image",dst)
-    cv2.waitKey(0) & 0xFF
-    cv2.destroyAllWindows()
+    return(dst)
+
+def process_image_grey(filename):
+    img = cv2.imread(filename)
+    gray_img= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return(gray_img)
+
 
 
